ae69e039e63450fe7aa016c50cb03eaa8b70d13a
Ben Hardill <ben@flowforge.com>
2026-09-16 11:09:30 +0100

    Try example.com

--- test/unit/nodes/project-link_spec.js
+++ test/unit/nodes/project-link_spec.js
@@ -39,7 +39,7 @@ describe('project-link node', function () {
         const RED = {
             settings: {
                 flowforge: {
-                    forgeURL: forgeUrl || 'https://www.hardill.me.uk',
+                    forgeURL: forgeUrl || 'https://local.example.com',
                     projectID: PROJECT_ID,
                     teamID: TEAM_ID,
                     projectLink: {
