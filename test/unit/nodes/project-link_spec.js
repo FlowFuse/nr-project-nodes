d6f4d2e4cda98d4c7f9c33e7d8fb1955c1dd8a7a
Ben Hardill <ben@flowforge.com>
2026-09-16 11:00:39 +0100

    fake push

--- test/unit/nodes/project-link_spec.js
+++ test/unit/nodes/project-link_spec.js
@@ -39,7 +39,7 @@ describe('project-link node', function () {
         const RED = {
             settings: {
                 flowforge: {
-                    forgeURL: forgeUrl || 'https://local.testfuse.com',
+                    forgeURL: forgeUrl || 'https://local-default.testfuse.com',
                     projectID: PROJECT_ID,
                     teamID: TEAM_ID,
                     projectLink: {
